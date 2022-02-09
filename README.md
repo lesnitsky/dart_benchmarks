8db4462386fb4561e60b599beaa52f690e254d60
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2022-02-09 19:49:55 +0300

    ⚡️

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dart benchmarks
+
+```sh
+# sync_future
+Future(RunTime): 15.675725510161959 us.
+SyncFutureGenerator(RunTime): 5.4679169970199855 us.
+```
+
+## Running benchmark
+
+```sh
+dart pub get
+dart run dart_benchmarks:sync_future <future|sync>
+```
+
+## LICENSE
+
+MIT
---
+++ bin/sync_future.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+import 'dart:async';
+
+import 'package:benchmark_harness/benchmark_harness.dart';
+import 'package:dart_benchmarks/syncronous_future.dart';
+
+abstract class Generator {
+  List<int> get values;
+  Future<int> generate();
+}
+
+class FutureGenerator implements Generator {
+  @override
+  final values = [];
+
+  @override
+  Future<int> generate() async {
+    return _r.nextInt(255);
+  }
+}
+
+class SyncFutureGenerator implements Generator {
+  @override
+  final values = [];
+
+  @override
+  Future<int> generate() {
+    return SynchronousFuture(_r.nextInt(255));
+  }
+}
+
+late Random _r;
+
+class Benchmark extends BenchmarkBase {
+  final Generator generator;
+  const Benchmark(String title, this.generator) : super(title);
+
+  static void main(String title, Generator generator) {
+    Benchmark(title, generator).report();
+  }
+
+  @override
+  void run() async {
+    final v = await generator.generate();
+    generator.values.add(v);
+  }
+
+  @override
+  void setup() {
+    _r = Random(255);
+  }
+
+  @override
+  void teardown() {}
+}
+
+void main(List<String> args) {
+  switch (args.first) {
+    case 'future':
+      Benchmark.main('Future', FutureGenerator());
+      break;
+
+    case 'sync':
+      Benchmark.main('SyncFutureGenerator', SyncFutureGenerator());
+      break;
+  }
+}
